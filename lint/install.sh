bae1e66e336118db0afdb0be2ea1e11ad8c1c783
kota999 <kota99949@gmail.com>
2018-09-08 04:53:05 +0900

    [Add] add textlint conf

---
+++ lint/install.sh
@@ -0,0 +1,5 @@
+npm install -g textlint
+npm install -g textlint-rule-preset-ja-technical-writing
+npm install -g textlint-filter-rule-comments
+npm install -g textlint-rule-spellcheck-tech-word
+npm install -g textlint-rule-no-todo
